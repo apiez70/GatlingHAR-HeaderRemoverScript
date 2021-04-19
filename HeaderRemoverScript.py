4a230e4b3ac8f2da9f2cddf4c8010e794760b0bc
apiez70 <53420427+apiez70@users.noreply.github.com>
2021-04-19 11:16:26 +0800

    Added foldername as argument
    Added new feature to add foldername as argument

--- HeaderRemoverScript.py
+++ HeaderRemoverScript.py
@@ -4,44 +4,49 @@
 import os
 import re
 import time 
-
+import sys
 #enter your directory by replacing FolderName below.
 #ensure all files are in scala format. no .txt 
 
 start = time.time()
-for subdir, dirs, files in os.walk('FolderName'):
-    for filename in files:
-        filepath = subdir + os.sep + filename
+if len(sys.argv) < 2:
+    print("Add folder name or edit the script; eg: 'python remover.py folderName' ")
+    exit(0)
+else:
+    var = sys.argv[1]
+    for subdir, dirs, files in os.walk(var):
+        for filename in files:
+            filepath = subdir + os.sep + filename
 
-        if filepath.endswith(".scala") :
-            print (filepath)
+            if filepath.endswith(".scala") :
+                print (filepath)
 
-            with open(filepath, "r") as lol:
-                
-                lines = lol.readlines()
-
-            with open(filepath, "w") as lol:
-                
-                for line in lines:
+                with open(filepath, "r") as lol:
                     
-                    if (re.search("val.head",line)):
-                        print("line val removed") #you can comment this to avoid spam
-                        #countval = countval + 1
+                    lines = lol.readlines()
 
-                    elif (re.search("->",line)):
-                        print("line -> removed") #you can comment this to avoid spam
+                with open(filepath, "w") as lol:
                     
-                    elif (re.search("\.headers\(headers_\d{1,3}\)",line)):
-                        lol.write(re.sub("\.headers\(headers_\d{1,3}\)","",line))
-                        print("removed headers") #you can comment this to avoid spam
+                    for line in lines:
+                        
+                        if (re.search("val.head",line)):
+                            print("line val removed") #you can comment this to avoid spam
+                            #countval = countval + 1
 
-                    else:
-                        #print(line) #you can comment this to avoid spam
-                        lol.write(line)
-        
-        print("Headers Removed - Done")
+                        elif (re.search("->",line)):
+                            print("line -> removed") #you can comment this to avoid spam
+                        
+                        elif (re.search("\.headers\(headers_\d{1,3}\)",line)):
+                            lol.write(re.sub("\.headers\(headers_\d{1,3}\)","",line))
+                            print("removed headers") #you can comment this to avoid spam
 
-end = time.time() #fancy stuff, not important
+                        else:
+                            #print(line) #you can comment this to avoid spam
+                            lol.write(line)
+            
+            print("Headers Removed - Done")
 
-print(end-start)
-print("seconds elapsed") #fancy stuff, not important
+    end = time.time() #fancy stuff, not important
+
+    print(end-start)
+    print("seconds elapsed") #fancy stuff, not important
